fbc80e7fe3690b6e4be25eb008b42db069e45cb3
Koichi Miyatake <atommy1966@users.noreply.github.com>
2022-05-07 09:30:45 +0000

    Initial commit

---
+++ main.jl
@@ -0,0 +1,32 @@
+import Dates
+# creating a Dates object 
+
+function Lucas_Lehmer_Test(p)
+  s = 4 #S_0の定義
+  m = (BigInt(1)<<p) - 1
+  
+  for n in 2:p-1
+#    s=(BigInt(s)^2-2)%m
+    s2=BigInt(s)^2
+    s = (s2 & m) + (s2 >> p)
+    if s >= m
+           s -= m
+    end
+    s -= 2
+  end
+  return s==0
+end
+
+    
+#M=0
+start=Dates.now()
+for i in 3:2:50000
+    M=Lucas_Lehmer_Test(UInt(i))
+  if M==true
+        println("p:",i)
+#        ans=2**i-#        println("2^p-1:",ans)        println("len(2^p-1):",len(str(ans)))
+#        println("2^p-1:",ans) 
+println("time:",Dates.now()-start)
+  end
+end
+
