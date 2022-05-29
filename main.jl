00c82706967726bb809294d87260eab31a01e02c
Koichi Miyatake <atommy1966@users.noreply.github.com>
2022-05-29 10:03:26 +0000

    Update source.

--- main.jl
+++ main.jl
@@ -3,13 +3,12 @@ import Pkg
 Pkg.add("Primes")
 import Primes:isprime,primes
 
-
 # creating a Dates object 
 function Lucas_Lehmer_Test(p)
   s = 4 #S_0の定義
   m = (BigInt(1)<<p) - 1
   
-  for n in 2:p-1
+  @inbounds @simd for n in 2:p-1
     s2=s^2
     s = (s2 & m) + (s2 >> p)
     if s >= m
@@ -22,13 +21,16 @@ end
 
     
 #M=0
-start=Dates.now()
-@inbounds @simd for p in primes(3,200000)
+startNumber=3
+endNumber=1000000
+p=startNumber
+@inbounds @simd for p in primes(startNumber,endNumber)
+  start=Dates.now()
     M=Lucas_Lehmer_Test(p)
   if M==true
         println("p:",p)
         ans=BigInt(2)^p-1
-        println("len(2^p-1):",length(string(ans)))
+        println("Digits of (2^p-1):",length(string(ans)))
 #        println("2^p-1:",ans) 
         println("time:",Dates.now()-start)
   end
